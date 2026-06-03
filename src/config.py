e04fc31af16079e572363a3541a40abd830b1ff9
Phantom <info@jishnu.tech>
2026-06-03 20:40:30 +0100

    config module

---
+++ src/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_TOKEN = os.getenv('CLIENT_TOKEN')
+GUILD_ID = os.getenv('GUILD_ID')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+
