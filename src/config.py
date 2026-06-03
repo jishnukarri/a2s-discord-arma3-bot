97f1f1409ec40d93c7c5b5da3fe2c2a753b18e73
Phantom <info@jishnu.tech>
2026-06-03 20:42:09 +0100

    logging setup added + config updated to remove duplicate

--- src/config.py
+++ src/config.py
@@ -6,6 +6,3 @@ load_dotenv()
 CLIENT_TOKEN = os.getenv('CLIENT_TOKEN')
 GUILD_ID = os.getenv('GUILD_ID')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
-
-CHANNEL_ID = os.getenv('CHANNEL_ID')
-
---
+++ src/logging.py
@@ -0,0 +1,13 @@
+import logging
+
+def setup_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    handler = logging.FileHandler('a2s-main.log')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s : %(message)s"
+    )
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
