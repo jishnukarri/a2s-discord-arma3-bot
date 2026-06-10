a8ed249b72cbbe8c8d64fe0be461787afab9097f
Phantom <info@jishnu.tech>
2026-06-10 19:11:29 +0100

    refactor: convert embed functions into class & added notes + changes to gitignore

--- src/embed.py
+++ src/embed.py
@@ -1,33 +1,44 @@
 import discord
 import datetime
 from tabulate import tabulate
-#Universal Lamda Functions
-title = lambda title: f"{title}'s Leaderboard" if (title) else "Leaderboard"
 
 
-"""
-Discord Embeds 
+class Embeds:
+    #Embed Functions
+    leaderboardTitle = lambda title: f"{title}'s Leaderboard" if (title) else "Leaderboard"
 
-createLeaderboard 
-parms: leaderboard:list, config: dict -> DiscordEmbed
-"""
+    def __init__(self,title,footer,footer_icon):
+        self.title = title
+        self.footer = footer
+        self.footer_icon = footer_icon
+        
+        
+    def createLeaderboard(self,leaderboard:list) -> discord.Embed:
+        """generates discord emebed
 
-def createLeaderboard(leaderboard:list, config:dict) -> discord.Embed:
-    
-    table = {"Name":[],"Kills":[],"Time Played":[]}
-    for row in leaderboard:
-        table["Name"].append(row[0])
-        table["Kills"].append(row[1])
-        table["Time Played"].append(row[2])
-    embed = discord.Embed(
-        title=title(config['title']), #Lambda Function - Title
-        timestamp=datetime.now())
-    embed.add_field(name="",
-                    value=f"""
-                    ```md
-                    {tabulate(table,headers="keys")}
-                    ```
-                    """)
-    if (config.get('footer')):
-        embed.set_footer(text=config.get('footer').text,icon_url=config.get('footer').image)
-    return embed
+        Args:
+            leaderboard (list): a list of tuples from sqliteDB from the 'database' DB
+
+        Returns:
+            discord.Embed: returns a discordEmebed
+        """        
+        
+        table = {"Name":[],"Kills":[],"Time Played":[]}
+        for row in leaderboard:
+            table["Name"].append(row[0])
+            table["Kills"].append(row[1])
+            table["Time Played"].append(row[2])
+        embed = discord.Embed(
+            title=self.leaderboardTitle(self.title), #Lambda Function - Title
+            timestamp=datetime.now())
+        embed.add_field(name="",
+                        value=f"""
+                        ```md
+                        {tabulate(table,headers="keys")}
+                        ```
+                        """)
+        if (self.footer):
+            embed.set_footer(text=self.footer,icon_url=self.footer_icon)
+        return embed
+    def createServerStatus(self,servers:list):
+        pass
