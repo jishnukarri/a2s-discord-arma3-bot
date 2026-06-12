6adc69d921ad58870ed7c0adc7b40d5e48d9c9a3
Phantom <info@jishnu.tech>
2026-06-12 19:36:02 +0100

    local folder move - pylance

---
+++ main.py
@@ -0,0 +1,19 @@
+# import a2s
+# from src.logging import setup_logging
+
+# # setup logging
+# setup_logging()
+
+# print("Hello from a2s-discord-bot!")
+# testInfo = a2s.info(("eu.rustreborn.gg", 28015))
+# print(type(testInfo))
+
+example = [
+    ('Jishnu', 125, '2026-09'),
+    ('Alex', 98, '2026-09'),
+    ('Bob', 45, '2026-10')
+]
+table = {"Name":{"A":0},"Kills":[],"Time Played":[]}
+
+
+print(table.get("Name")["A"])
