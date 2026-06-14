9e29d6616ebd62bbefed370d450c06d8a6a546a6
Phantom <info@jishnu.tech>
2026-06-14 16:25:42 +0100

    added env variables for emebeds

--- src/config.py
+++ src/config.py
@@ -1,9 +1,20 @@
 import os
+import requests
 from dotenv import load_dotenv
 
+
+"""
+
+LOCAL ENVIROMENT VARIBLES IMPORT
+
+"""
 load_dotenv()
 
 CLIENT_TOKEN = os.getenv("CLIENT_TOKEN")
-GUILD_ID = os.getenv("GUILD_ID")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
+GUILD_ID = int(os.getenv("GUILD_ID"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 DATABASE_FILE = os.getenv("DATABASE_FILE")
+RAW_REPO_LINK = os.getenv("RAW_REPO_LINK")
+TITLE = os.getenv("TITLE")
+FOOTER = os.getenv("FOOTER")
+FOOTER_ICON = os.getenv("FOOTER_ICON")
