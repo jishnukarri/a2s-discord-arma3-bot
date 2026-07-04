d174a239c0026514f123dfb75984fab6409e9fb8
phantom <jishnu.karri@gmail.com>
2026-07-04 22:08:51 +0100

    added local test file

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-import src.config as config
+from src.config import CLIENT_TOKEN
+from src.bot.main import *
+import discord
+import logging
 
-print(config.addToDatabase(["hello"],50))
+# 1. Setup basic logging configuration for the terminal
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+intents = discord.Intents.default()
+
+bot = DiscordBot(intents=intents)
+bot.run(CLIENT_TOKEN)
